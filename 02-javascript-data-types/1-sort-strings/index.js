5c52a0eec3021cd135ba65189478537e9f465d7b
Ivan <ivan@mac.lan>
2026-06-03 02:38:05 -0700

    feat: implement sortStrings function

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,15 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const supportedLanguages = ['ru', 'en'];
 
+  const options = {
+    caseFirst: "upper",
+  };
+
+  const collator = new Intl.Collator(supportedLanguages, options);
+
+  const sortedArray = arr.toSorted(collator.compare);
+
+  return param === 'asc' ? sortedArray : sortedArray.reverse();
 }

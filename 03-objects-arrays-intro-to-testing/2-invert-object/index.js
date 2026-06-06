5d4d3b24e0e5978855b487ba385c78959acb60e2
hydoken <20961928+hydoken@users.noreply.github.com>
2026-06-06 00:43:09 -0700

    refactor: improve invertObj implementation

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,11 +4,11 @@
  * @returns {object | undefined} - returns the new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-  if (obj === undefined) {
+  if (!obj) {
     return undefined;
   }
 
   const entries = Object.entries(obj);
 
-  return Object.fromEntries(entries.map(entry => entry.reverse()));
+  return Object.fromEntries(entries.map(([key, value]) => [value, key]));
 }

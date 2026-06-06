120ea13bb03591cc6d802a78e0895289bcde99c6
hydoken <20961928+hydoken@users.noreply.github.com>
2026-06-06 00:36:16 -0700

    refactor: improve sortStrings implementation

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,19 +1,14 @@
+const collator = new Intl.Collator(["ru", "en"], { caseFirst: "upper" });
+
 /**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-  const supportedLanguages = ['ru', 'en'];
 
-  const options = {
-    caseFirst: "upper",
-  };
-
-  const collator = new Intl.Collator(supportedLanguages, options);
-
+export function sortStrings(arr, param = "asc") {
   const sortedArray = arr.toSorted(collator.compare);
 
-  return param === 'asc' ? sortedArray : sortedArray.reverse();
+  return param === "asc" ? sortedArray : sortedArray.reverse();
 }
